a8cc7b1e804f1c33f63f45fff5679942dfe8042a
Ace Gamitin <gamitinacevincent@gmail.com>
2025-05-16 03:21:58 +0800

    #

--- main.py
+++ main.py
@@ -20,7 +20,35 @@ class main(QMainWindow):
                                                     'studio_name'])
         self.loaddata()
 
-
+    def loaddata(self):
+        con = mysql.connector.connect(user = 'root', 
+                                        password = 'CS2025EU', 
+                                        host = 'localhost', 
+                                        database= 'movie_manager')
+        cur = con.cursor()
+        sqlstr = '''SELECT 
+                        m.movie_id,
+                        m.movie_name,
+                        m.release_year,
+                        g.genre_name,
+                        s.studio_name
+                    FROM 
+                        movie m
+                    JOIN 
+                        genre g ON m.genre_id = g.genre_id
+                    JOIN 
+                        studio s ON m.studio_id = s.studio_id
+                    ORDER BY 
+                        m.movie_id;'''
+        
+        tablerow=0
+        results = cur.execute(sqlstr)
+        self.tableWidget.setRowCount(40)
+        for row in results:
+            self.tableWidget.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(row[0]))
+            self.tableWidget.setItem(tablerow, 1, QtWidgets.QTableWidgetItem(row[1]))
+            self.tableWidget.setItem(tablerow, 2, QtWidgets.QTableWidgetItem(row[2]))
+            tablerow+=1
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
