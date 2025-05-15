9e5afe337328675e7db2c0240bbb45374432effb
Ace Gamitin <gamitinacevincent@gmail.com>
2025-05-16 03:15:43 +0800

    #

--- main.py
+++ main.py
@@ -8,3 +8,22 @@ class main(QMainWindow):
     def __init__(self):
         super(main, self).__init__()
         loadUi("main.ui", self)
+        self.tableWidget.setColumnWidth(0,100)
+        self.tableWidget.setColumnWidth(1,250)
+        self.tableWidget.setColumnWidth(2,100)
+        self.tableWidget.setColumnWidth(3,100)
+        self.tableWidget.setColumnWidth(4,215)
+        self.tableWidget.setHorizontalHeaderLabels(['movie_id',
+                                                    'movie_name',
+                                                    'release_year',
+                                                    'genre_name',
+                                                    'studio_name'])
+        self.loaddata()
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui = main()
+    ui.show()
+    app.exec_()
