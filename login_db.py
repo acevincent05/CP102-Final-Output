9bdb64f55b11ee96ca755d3989b1e7141cf7fe37
Ace Gamitin <gamitinacevincent@gmail.com>
2025-05-16 00:24:38 +0800

    #

--- login_db.py
+++ login_db.py
@@ -1,3 +1,18 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.uic import loadUi
 import sys
+
+class MainUI(QMainWindow):
+    def __init__(self):
+        super(MainUI, self).__init__()
+        loadUi("mainui.ui", self)
+        self.pushButton.clicked.connect(self.clickhandler)
+
+    def clickhandler(self):
+        print("Hello World")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui = MainUI()
+    ui.show()
+    app.exec_()
