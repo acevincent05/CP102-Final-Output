f417efb65fe58a8b091fa7401fcfa769fb367a9c
Ace Gamitin <gamitinacevincent@gmail.com>
2025-05-16 21:22:17 +0800

    #

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QTableWidgetItem, QTableWidget, QLabel, QDialog, QFormLayout, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QTableWidgetItem, QTableWidget, QLabel
 from PyQt5.uic import loadUi
 import sys
 from connect_database import ConnectDatabase
+from MovieDetailsDialog import MovieDetailsDialog
 
 class main_window(QMainWindow):
     def __init__(self):
@@ -177,41 +178,6 @@ class main_window(QMainWindow):
 
         except Exception as e:
             QMessageBox.critical(self, "Error", f"Could not load data:\n{str(e)}")
-
-class MovieDetailsDialog(QDialog):
-    def __init__(self, movie_data, parent=None):
-        super().__init__(parent)
-        self.setWindowTitle("Movie Details")
-        self.setModal(True)
-        self.resize(400, 300)  # Increased size to accommodate more fields
-        
-        layout = QFormLayout()
-        
-        # Movie Information Section
-        layout.addRow(QLabel("<b>Movie Information</b>"))
-        layout.addRow("ID:", QLabel(str(movie_data[0])))
-        layout.addRow("Title:", QLabel(str(movie_data[1])))
-        layout.addRow("Release Year:", QLabel(str(movie_data[2])))
-        
-        # Genre Information Section
-        layout.addRow(QLabel("<b>Genre Information</b>"))
-        layout.addRow("Genre ID:", QLabel(str(movie_data[3])))
-        layout.addRow("Genre Name:", QLabel(str(movie_data[4])))
-        
-        # Studio Information Section
-        layout.addRow(QLabel("<b>Studio Information</b>"))
-        layout.addRow("Studio Name:", QLabel(str(movie_data[5])))
-        layout.addRow("Studio ID:", QLabel(str(movie_data[6])))
-        layout.addRow("Founded Year:", QLabel(str(movie_data[7])))
-        layout.addRow("Headquarters:", QLabel(str(movie_data[8])))
-        
-        # Add a close button
-        close_btn = QPushButton("Close")
-        close_btn.clicked.connect(self.close)
-        layout.addRow(close_btn)
-        
-        self.setLayout(layout)
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
